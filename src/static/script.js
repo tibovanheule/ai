6949fcd8cb54608d80b3b9fcf15cbbd202fc178b
Tibo Vanheule <tibo@tibovanheule.space>
2020-10-16 20:22:23 +0200

    nicer idents + promisses in js script

--- src/static/script.js
+++ src/static/script.js
@@ -1,78 +1,81 @@
 function fadeOut(element) {
-    let op = 1;
-    let timer = setInterval(() => {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }else{
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.2;
-	}
-    }, 40);
+    return new Promise((resolve, reject) => {
+        try {
+            let op = 1;
+            let timer = setInterval(() => {
+                if (op <= 0.1) {
+                    clearInterval(timer);
+                    element.style.display = 'none';
+                    resolve();
+                } else {
+                    element.style.opacity = op;
+                    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                    op -= op * 0.2;
+                }
+            }, 40);
+        } catch (e) {
+            reject()
+        }
+    })
+
 }
 
 function fadeIn(element) {
-    element.style.display = 'block'
-    let op = 0.1;  // initial opacity
-    let timer = setInterval(() => {
-        if (op > 0.95){
-            clearInterval(timer);
-	    element.style.opacity = 1;
-        }else{
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.2;
-	}
-    }, 40);
+    return new Promise((resolve, reject) => {
+        try {
+            element.style.opacity = '0';
+            element.style.display = 'block'
+            let op = 0.1;  // initial opacity
+            let timer = setInterval(() => {
+                if (op > 0.95) {
+                    clearInterval(timer);
+                    element.style.opacity = 1;
+                } else {
+                    element.style.opacity = op;
+                    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                    op += op * 0.2;
+                }
+            }, 40);
+        } catch (e) {
+            reject()
+        }
+    })
 }
 
-function analyse(){
-	if(document.getElementById("tweet").textLength){
-	fadeOut(document.getElementById("text_input"))
-	
-	fetch('https://ai.tibovanheule.space/api/analyse',{
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-       body: JSON.stringify({message:document.getElementById("tweet").value}) // body data type must match "Content-Type" header
-  })
-  .then(data=> data.text())
-  .then(data => {
-console.log(data);
-document.getElementById("response").innerHTML = data;
-fadeIn(document.getElementById("validate"));
-});
-	
-	
-	}
+const analyse = async () => {
+    if (document.getElementById("tweet").textLength) {
+        await fadeOut(document.getElementById("text_input"));
+        fetch('https://ai.tibovanheule.space/api/analyse', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({message: document.getElementById("tweet").value})
+        }).then(data => data.text()).then(data => {
+            console.log(data);
+            document.getElementById("response").innerHTML = data;
+            fadeIn(document.getElementById("validate"));
+        });
+    }
 }
 
 
-function validate(bool){
-
-	
-	fetch('https://ai.tibovanheule.space/api/validate',{
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-       body: JSON.stringify({validate:bool, message:document.getElementById("tweet").value}) // body data type must match "Content-Type" header
-  })
-  .then(data=> data.text())
-  .then(data => {
-console.log(data);
-document.getElementById("validatingtext").innerHTML = "Thank you for your response";
-fadeOut(document.getElementById("buttons"));
-});
-	
-	
-	
+const validate = async (bool) => {
+    fetch('https://ai.tibovanheule.space/api/validate', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({validate: bool, message: document.getElementById("tweet").value})
+    }).then(data => data.text()).then(data => {
+        console.log(data);
+        document.getElementById("validatingtext").innerHTML = "Thank you for your response";
+        fadeOut(document.getElementById("buttons"));
+    });
 }
