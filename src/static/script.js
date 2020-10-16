76cbfad640a67bc6dc9e820dbb03322795b975a3
Tibo Vanheule <tibo@tibovanheule.space>
2020-10-16 19:00:13 +0200

    voor test static

---
+++ src/static/script.js
@@ -0,0 +1,47 @@
+function fadeOut(element) {
+    let op = 1;
+    let timer = setInterval(() => {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }else{
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.2;
+	}
+    }, 40);
+}
+
+function fadeIn(element) {
+    element.style.display = 'block'
+    let op = 0.1;  // initial opacity
+    let timer = setInterval(() => {
+        if (op > 0.95){
+            clearInterval(timer);
+	    element.style.opacity = 1;
+        }else{
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.2;
+	}
+    }, 40);
+}
+
+function analyse(){
+	if(document.getElementById("tweet").textLength){
+	fetch('https://ai.tibovanheule.space/api/analyse',{
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+       body: JSON.stringify({message:document.getElementById("tweet").value}) // body data type must match "Content-Type" header
+  })
+  .then(response => response.json())
+  .then(data => console.log(data));
+	fadeOut(document.getElementById("text_input"))
+	fadeIn(document.getElementById("validate"))
+	}
+}
