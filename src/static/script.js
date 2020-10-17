52faccd43aea17ea67b4f2532b490ea633a54aff
Tibo Vanheule <tibo@tibovanheule.space>
2020-10-17 14:26:41 +0200

    small fixes

--- src/static/script.js
+++ src/static/script.js
@@ -45,6 +45,7 @@ function fadeIn(element) {
 
 const analyse = async () => {
     if (document.getElementById("tweet").textLength) {
+        document.getElementById("anabutton").disabled = true;
         await fadeOut(document.getElementById("text_input"));
         await fadeIn(document.getElementById("responseholder"))
         fetch('https://ai.tibovanheule.space/api/analyse', {
@@ -59,13 +60,16 @@ const analyse = async () => {
         }).then(data => data.text()).then(data => {
             console.log(data);
             document.getElementById("response").innerHTML = data;
+
+
             fadeIn(document.getElementById("validate"));
         });
     }
 }
 
 
-const validate = async (bool) => {
+const validate = async (bool) => {document.getElementById("yes").disabled = true;
+    document.getElementById("no").disabled = true;
     fetch('https://ai.tibovanheule.space/api/validate', {
         method: 'POST',
         mode: 'cors',
