415565d3fb0ae22254eb30869dd7c65f302889fd
Tibo Vanheule <tibo@tibovanheule.space>
2020-11-10 11:31:30 +0100

    from + data load

--- src/ai.py
+++ src/ai.py
@@ -3,16 +3,16 @@ The Ai module implements the core ai functions
 
 More details....
 """
-import NLP
-from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from NLP import text_precessing
 
 
-def analyse_text(text):
-
+def analyse_text(text,data):
     # print(str(NLP.text_precessing(text)))
     stopwords_set = stopwords.words("english")
-    vectorizer = TfidfVectorizer(preprocessor=NLP.text_precessing, tokenizer=return_token, stop_words=stopwords_set,
+    vectorizer = TfidfVectorizer(preprocessor=text_precessing, tokenizer=return_token, stop_words=stopwords_set,
                                  max_df=0.75, min_df=5, use_idf=True, smooth_idf=False, ngram_range=(1, 3), norm=None,
                                  decode_error="replace")
 
@@ -21,7 +21,7 @@ def analyse_text(text):
 
 
 def process_text(text):
-    return str(NLP.text_precessing(text))
+    return str(text_precessing(text))
 
 
 def return_token(text):
@@ -35,6 +35,6 @@ def validate():
 def construct_matrix(text, vecto):
     # ? tf is this even used for???
     tfidf = vecto.fit_transform(text).toarray()
-    vocab = {v:i for i,v in enumerate(vecto.get_feature_names())}
+    vocab = {v: i for i, v in enumerate(vecto.get_feature_names())}
     idf_vals = vecto.idf_
     return text
--- src/server.py
+++ src/server.py
@@ -4,18 +4,22 @@ Provides the api routes and calls ai functions. With this we can implement an we
 More details.
 """
 
+from demoji import download_codes
 from flask import Flask, jsonify, request
+from nltk import download
+
 import ai
-import nltk
-import demoji
+from db import DB
 
 app = Flask(__name__)
+database = DB()
+data = database.db_load()
 
 
 @app.route('/api/analyse', methods=['GET', 'POST'])
 def analyze():
     if request.method == 'POST':
-        return ai.analyse_text(request.json["message"])
+        return ai.analyse_text(request.json["message"],data)
     else:
         return jsonify("Hello, this is the ai speaking. the ai hate you already and you are going to hate it :) ")
 
@@ -39,8 +43,13 @@ def process():
 
 @app.route('/api/init', methods=['GET'])
 def init():
-    nltk.download('wordnet')
-    nltk.download('stopwords')
-    nltk.download('averaged_perceptron_tagger')
-    demoji.download_codes()
+    download('wordnet')
+    download('stopwords')
+    download('averaged_perceptron_tagger')
+    download_codes()
     return str("inited")
+
+
+@app.route('/api/data', methods=['GET'])
+def showdata():
+    return str(data)
