54a4a1bcfc733ff5395a43fa00b8eeb638ac9f49
Tibo Vanheule <tibo@tibovanheule.space>
2020-12-16 00:10:27 +0100

    bug fixes

--- src/db.py
+++ src/db.py
@@ -103,10 +103,13 @@ class DB:
             next(read)
             for i in read:
                 # don't wont every non-hate, but some to test
-                if float(i[-1]) <= 0.5 and random.random() < 0.25:
-                    i[-1] = "0"
-                    print("insert")
-                    self.insert_ad(i, cursor)
+                if float(i[-1]) <= 0.5:
+                    if random.random() < 0.25:
+                        i[-1] = "0"
+                        print("insert")
+                        self.insert_ad(i, cursor)
+                    else:
+                        continue
                 # hate speech do something
                 else:
                     i[-1] = "1"
@@ -125,13 +128,12 @@ class DB:
                                                 range(random.randint(2, (len(tweet) % 4) + 2)))
                                 new = left + k + right
                                 tweet = tweet.replace(k, new, 1)
-                    for _ in [0, 1]:
-                        if random.random() < 0.5:
-                            r = random.randint(1, len(tweet) // 2)
-                            tweet = tweet[:r] + tweet[r:].replace(' ', "", 1)
-                        if random.random() > 0.5:
-                            r = random.randint(1, len(tweet) - 1)
-                            tweet = tweet[:r] + ' ' + tweet[r:]
+                    if random.random() < 0.5:
+                        r = random.randint(1, len(tweet) // 2)
+                        tweet = tweet[:r] + tweet[r:].replace(' ', "", 1)
+                    if random.random() > 0.5:
+                        r = random.randint(1, len(tweet) - 1)
+                        tweet = tweet[:r] + ' ' + tweet[r:]
                     i[0] = tweet
                     print(i[0])
                     self.insert_ad(i, cursor)
