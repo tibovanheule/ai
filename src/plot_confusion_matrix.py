53b38af052abd2923aa6948b48b7e1b391c3d50f
Tibo Vanheule <tibo@tibovanheule.space>
2020-12-16 10:51:29 +0100

    presentation

--- src/plot_confusion_matrix.py
+++ src/plot_confusion_matrix.py
@@ -4,7 +4,7 @@ import seaborn as sn
 
 
 def plot():
-    array = [[0.96, 0.04], [0.74, 0.26]]
+    array = [[0.66, 0.34], [ 0.55 , 0.45]]
     df_cm = pd.DataFrame(array, index=[i for i in "01"], columns=[i for i in "01"])
     plt.figure(figsize=(10, 7))
     sn.heatmap(df_cm, annot=True)
